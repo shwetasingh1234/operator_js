01ac43a1ffeb01ae2789529a9093da1059d887da
shwetasingh1234 <shweta7393ss@gmail.com>
2026-02-04 10:53:46 +0530

    here using the increament operator

--- assignment.js
+++ assignment.js
@@ -1,5 +1,9 @@
 let a=4;
 let b;
-console.log("before using assingment operator a",a);
+console.log("before using assingment operator a=",a);
 console.log("now assingment operator operate on two variable ");
-console.log("after using assingment operator b",b=a);
+console.log("after using assingment operator b=",b=a);
+//here we are using increament operator
+let x=2;
+x+=1;
+console.log("print the x=",x);
