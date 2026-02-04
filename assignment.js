5ef7d8798b17c98cc1a057cdc7cdee27d3d59c07
shwetasingh1234 <shweta7393ss@gmail.com>
2026-02-04 10:38:32 +0530

    adding assignment operator

---
+++ assignment.js
@@ -0,0 +1,5 @@
+let a=4;
+let b;
+console.log("before using assingment operator a",a);
+console.log("now assingment operator operate on two variable ");
+console.log("after using assingment operator b",b=a);
